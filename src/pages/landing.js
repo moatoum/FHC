9abf0e0ce40fc80916520e53d732f9d720c3edfd
Mohammed Atoum <moatoum@gmail.com>
2024-09-30 12:56:45 +0300

    Landing page update #3

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,41 +1,25 @@
 import React, { useEffect } from 'react';
-import Header from '../components/header';
-import Footers from '../components/footers';
-import SlideOne from '../components/landing/slideOne';
-import SlideTwo from '../components/landing/slideTwo';
-import SlideThree from '../components/landing/slideThree';
-import SlideFour from '../components/landing/slideFour';
-import SlideFive from '../components/landing/slideFive';
-import SlideSix from '../components/landing/slideSix';
 import { serverURL } from '../constants';
 import axios from 'axios';
 
 const Landing = () => {
-
     useEffect(() => {
         async function dashboardData() {
             const postURL = serverURL + `/api/policies`;
             const response = await axios.get(postURL);
             sessionStorage.setItem('TermsPolicy', response.data[0].terms);
-            sessionStorage.setItem('PrivacyPolicy', response.data[0].privacy)
+            sessionStorage.setItem('PrivacyPolicy', response.data[0].privacy);
         }
         if (sessionStorage.getItem('TermsPolicy') === null && sessionStorage.getItem('PrivacyPolicy') === null) {
             dashboardData();
         }
+
+        // Redirect to the external URL
+        window.location.href = 'https://fikrahub.com/public/courses';
     }, []);
 
-    return (
-        <>
-            <Header isHome={false} />
-            <SlideOne />
-            <SlideTwo />
-            <SlideThree />
-            <SlideFour />
-            <SlideFive />
-            <SlideSix />
-            <Footers />
-        </>
-    );
+    // Return null or a loading indicator since this component will redirect immediately
+    return null;
 };
 
 export default Landing;
