958ab1bb189d34074e5c36dfd24a612b8b01fa33
Lucas Vencenzi <lucas@netygo.com>
2024-12-08 18:37:14 +0700

    Update image to normal

--- server/server.js
+++ server/server.js
@@ -376,7 +376,7 @@ app.post('/api/image', async (req, res) => {
 
     const opts = {
         searchTerm: promptString,
-        queryStringAddition: '&imgsz=small',
+        queryStringAddition: '&imgsz=normal',
         filterOutDomains: ['gstatic.com'],
         userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36',  // User-Agent tùy chỉnh
     };
